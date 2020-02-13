07a16a0e34c01a2c4affed9f5df2a2c5417b83a9
diversifiedmechanic <diversifiedmechanic01@gmail.com>
2020-02-12 20:34:02 -0700

    Finalizing the set up for the API endpoints.

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 const PORT = 3300;
-const Review = require('../database');
+const db = require('../database/mongo');
 var cors = require('cors');
 
 app.use(express.static('./public'));
@@ -14,7 +14,7 @@ app.listen(PORT, () => {
 });
 
 app.get('/:restaurantId/', (req, res) => {
-  Review.find(req.params).sort('-dineDate')
+  db.getRestaurantReviews(req.params)
     .then((data) => {
       res.status(200).send(data);
     })
@@ -26,24 +26,65 @@ app.get('/:restaurantId/', (req, res) => {
 app.get('/sort/:id/:sorting/:list/', (req, res) => {
   const list = JSON.parse(req.params.list);
   let sortField = (req.params.sorting === 'Highest') ? '-overall' : 'overall';
-  if (req.params.sorting === 'Newest' || req.params.sorting === 0) {
-    sortField = '-dineDate';
-  }
-  if (!list.length && sortField !== '-dineDate') {
-    Review.find({ restaurantId: req.params.id }).sort(sortField).sort('-dineDate')
-      .then((data) => {
-        res.status(200).send(data);
-      })
-      .catch((err) => {
-        console.log('error getting data from database ', err);
-      });
-  } else {
-    Review.find({ restaurantId: req.params.id, filterTag: { $in: list } }).sort(sortField).sort('-dineDate')
-      .then((data) => {
-        res.status(200).send(data);
-      })
-      .catch((err) => {
-        console.log('error getting data from database ', err);
-      });
-  }
+  db.getSortedRestaurantReviews({
+    sortField: sortField,
+    sorting: req.params.sorting,
+    restaurantId: req.params.id,
+    list: list,
+  })
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      console.log('error getting data from database ', err);
+    });
+});
+
+app.post('/api/reviews', (req, res) => {
+  const newReview = req.body;
+  db.addOneReview(newReview)
+    .then((addedReview) => {
+      res.status(200).json(addedReview);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json(error);
+    });
+});
+
+app.get('/api/reviews/:reviewId', (req, res) => {
+  db.getOneReview(req.params.reviewId)
+    .then((review) => {
+      res.status(200).json(review);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json(error);
+    });
+});
+
+app.put('/api/reviews', (req, res) => {
+  const updatedReviewData = req.body;
+  const reviewId = req.body._id;
+  delete updatedReviewData._id;
+
+  db.updateOneReview(reviewId, updatedReviewData)
+    .then((updatedReview) => {
+      res.status(200).json(updatedReview);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json(error);
+    });
+});
+
+app.delete('/api/reviews/:reviewId', (req, res) => {
+  db.deleteOneReview(req.params.reviewId)
+    .then((deletedReview) => {
+      res.status(200).json(deletedReview);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json(error);
+    });
 });

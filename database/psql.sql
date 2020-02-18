656851de2ece32e78a5db3865097f989361c6fb5
diversifiedmechanic <diversifiedmechanic01@gmail.com>
2020-02-17 19:12:25 -0700

    File to be run from terminal to import the CSV files.

---
+++ database/psql.sql
@@ -0,0 +1,46 @@
+-- reviewers
+
+DROP TABLE IF EXISTS reviewers;
+
+CREATE TABLE reviewers (
+  id  BIGINT PRIMARY KEY,
+  firstName TEXT,
+  lastName TEXT,
+  color TEXT NOT NULL,
+  vip BOOLEAN NOT NULL
+);
+
+COPY reviewers(id, color, vip, firstName, lastName)
+FROM '/home/mrclean/HRR43/SDC/reviews/CSV/Reviewers.csv' DELIMITER ',' CSV HEADER;
+
+
+DROP TABLE IF EXISTS restaurants;
+
+CREATE TABLE restaurants (
+  id BIGINT PRIMARY KEY,
+  city TEXT NOT NULL
+);
+
+COPY restaurants (id, city)
+FROM '/home/mrclean/HRR43/SDC/reviews/CSV/Restaurants.csv' DELIMITER ',' CSV HEADER;
+
+DROP TABLE IF EXISTS reviews;
+
+CREATE TABLE reviews (
+  id BIGINT PRIMARY KEY,
+  restaurantId BIGINT NOT NULL,
+  reviewerId BIGINT NOT NULL,
+  overall INT NOT NULL,
+  food INT NOT NULL,
+  service INT NOT NULL,
+  ambience INT NOT NULL,
+  dineDate DATE NOT NULL,
+  noise INT NOT NULL,
+  recommend BOOLEAN NOT NULL,
+  filterTag TEXT,
+  comments TEXT
+);
+
+
+COPY reviews (id, restaurantId, reviewerId, overall, food, service, ambience, dineDate, noise, recommend, filterTag, comments)
+FROM '/home/mrclean/HRR43/SDC/reviews/CSV/Reviews.csv' DELIMITER ',' CSV HEADER;
